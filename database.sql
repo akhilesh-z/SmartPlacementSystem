24f100a8b1df14d6609123f945e8a6e610a7072e
Akhilesh <www.akhi2244@gmail.com>
2026-06-29 14:27:17 +0530

    Added README and database Scripts

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE placement_db;
+USE placement_db;
+
+CREATE TABLE admin(...);
+
+CREATE TABLE students(...);
+
+CREATE TABLE companies(...);
+
+CREATE TABLE jobs(...);
+
+CREATE TABLE applications(...);
+
+INSERT INTO admin(username,password)
+VALUES('admin','admin123');
